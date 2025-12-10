fe94eb424025a99caa988048a7e2845c2136c80f
Akansha <akansha887731@gmail.com>
2025-12-10 09:20:46 +0530

    Updated Merge_SOrted_Array.py

--- Merge_Sorted_Array.py
+++ Merge_Sorted_Array.py
@@ -37,23 +37,45 @@ nums2.length == n
 """
 
 
-class Solution(object):
+class Solution:
     def merge(self, nums1, m, nums2, n):
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        i, j, k = m-1, n-1, m+n-1
-        
-        while (j>=0):
-            
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i] 
-                i-=1
+        position = m+n-1
+        i = n-1
+        j = m-1
+
+        while i >= 0 and j >= 0:
+            if nums2[i] > nums1[j]:
+                nums1[position] = nums2[i]
+                i -= 1
             else:
-                nums1[k] = nums2[j]
-                j-=1
-            k-=1
+                nums1[position] = nums1[j]
+                j -= 1
+            position -= 1
+
+        if i > j:
+            nums1[:position+1] = nums2[:i+1]
+            
+        return nums1 
+    
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3  
+
+obj = Solution()
+result = obj.merge(nums1, m, nums2, n)
+print(result)  # Output: [1,2,2,3,5,6]
+
+
+nums1 = [2,0]
+m = 1
+nums2 = [1]
+n = 1  
+
+obj = Solution()
+result = obj.merge(nums1, m, nums2, n)
+print(result)  # Output: [1,2]
